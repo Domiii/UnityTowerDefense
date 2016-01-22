a2e9d9d432dad0d4b367fb223a01c5994aed99a7
Domi <dominik.seifert@gmail.com>
2016-01-22 12:44:52 +0800

    - minor updates

---
+++ Assets/Scripts/SpriteRepeater.cs
@@ -0,0 +1,16 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class SpriteRepeater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var spriteRenderer = GetComponent<SpriteRenderer> ();
+		spriteRenderer.sprite.texture.wrapMode = TextureWrapMode.Repeat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
