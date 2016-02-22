ef0a01ece8242b615c718a17ea6cd4749c694d1b
domi <dominik.seifert@gmail.com>
2016-02-22 19:49:39 +0800

    - work on menu system

---
+++ Assets/Scripts/Editor/SwitchSceneButtonEditor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Linq;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(SwitchSceneButton))]
+public class SwitchSceneButtonEditor : Editor {
+	IEnumerable<string> GetAllSceneNames() {
+		foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+		{
+			if (scene.enabled)
+			{
+				string name = scene.path.Substring(scene.path.LastIndexOf('/')+1);
+				name = name.Substring(0,name.Length-6);
+				yield return name;
+			}
+		}
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		Debug.Log (EditorBuildSettings.scenes.Length);
+		base.OnInspectorGUI ();
+
+		var btn = (SwitchSceneButton)target;
+
+		EditorGUILayout.LabelField ("Scene");
+
+		var scenes = GetAllSceneNames ().ToArray();
+		var currentIndex = System.Array.IndexOf(scenes, btn.Scene);
+
+		// display list
+		currentIndex = EditorGUILayout.Popup(currentIndex, scenes);
+		
+		if (currentIndex > -1) {
+			btn.Scene = scenes[currentIndex];
+		}
+	}
+}
---
+++ Assets/Scripts/SwitchSceneButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchSceneButton : MonoBehaviour {
+	[HideInInspector]
+	public string scene;
+	public string Scene {
+		get {
+			return scene;
+		}
+		set {
+			scene = value;
+		}
+	}
+
+	void OnMouseDown() {
+		Application.LoadLevel (Scene);
+	}
+}
