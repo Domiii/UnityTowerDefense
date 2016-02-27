3243c588579ea09d81251eca2035e5929ff62205
Domi <dominik.seifert@gmail.com>
2016-02-27 22:42:48 +0800

    - some more cleanup + corrected BuyUnitButtong preview scaling

--- Assets/Scripts/BuyUnitConfig.cs
+++ Assets/Scripts/BuyUnitConfig.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 [System.Serializable]
 public class BuyUnitConfig {
+	public bool Enabled = true;
 	public int CreditCost;
 	public GameObject UnitPrefab;
 	public float CooldownSeconds;
